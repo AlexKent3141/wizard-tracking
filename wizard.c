1bf0891924bb96df9ec2e72423a7f40f5e3a058c
Alex Kent <alex.kent@ultraleap.com>
2024-07-02 14:46:19 +0800

    Implemented tracking loop.
    Only keep track of the latest tracking data.

--- wizard.c
+++ wizard.c
@@ -1,8 +1,52 @@
+#include "assert.h"
 #include "LeapC.h"
 #include "ncurses.h"
+#include "stdatomic.h"
 #include "stdlib.h"
+#include "string.h"
+#include "threads.h"
 
 int max_x, max_y;
+atomic_int quit = 0;
+
+mtx_t tracking_mtx;
+atomic_int received_tracking = 0;
+
+struct tracking_data
+{
+  uint32_t num_hands;
+  LEAP_HAND hands[2];
+} latest_tracking;
+
+int poll_tracking(void* data)
+{
+  LEAP_CONNECTION conn;
+  assert(LeapCreateConnection(NULL, &conn) == eLeapRS_Success);
+  assert(LeapOpenConnection(conn) == eLeapRS_Success);
+
+  while (!quit)
+  {
+    LEAP_CONNECTION_MESSAGE msg;
+    eLeapRS res = LeapPollConnection(conn, 100, &msg);
+    if (res != eLeapRS_Success) continue;
+
+    // Only interested in the latest tracking event.
+    if (msg.type == eLeapEventType_Tracking)
+    {
+      assert(mtx_lock(&tracking_mtx) == thrd_success);
+
+      int num_hands = msg.tracking_event->nHands;
+      latest_tracking.num_hands = num_hands;
+      memcpy(
+        latest_tracking.hands,
+        msg.tracking_event->pHands,
+        num_hands * sizeof(LEAP_HAND));
+
+      assert(mtx_unlock(&tracking_mtx) == thrd_success);
+      received_tracking = 1;
+    }
+  }
+}
 
 struct state
 {
@@ -23,6 +67,14 @@ void render(const struct state* s)
   box(stdscr, 0, 0);
 
   mvwaddch(stdscr, s->hand_x, s->hand_y, '#');
+
+  // Latest tracking data.
+  if (received_tracking)
+  {
+    assert(mtx_lock(&tracking_mtx) == thrd_success);
+    mvprintw(2, 2, "Tracking: %d\n", latest_tracking.num_hands);
+    assert(mtx_unlock(&tracking_mtx) == thrd_success);
+  }
 }
 
 int main()
@@ -34,11 +86,16 @@ int main()
 
   getmaxyx(stdscr, max_x, max_y);
 
+  // Kick off the tracking thread.
+  mtx_init(&tracking_mtx, mtx_plain);
+  thrd_t tid;
+  thrd_create(&tid, &poll_tracking, NULL);
+
   struct state s = {};
-  for (;;)
+  while (!quit)
   {
     int key = wgetch(stdscr);
-    if (key == 'q') break;
+    quit = key == 'q';
 
     napms(100);
 
@@ -48,6 +105,8 @@ int main()
     render(&s);
     refresh();
   }
+
+  thrd_join(tid, NULL);
 
   endwin();
 
